b1944db9aece23a20243a9948845730aa9183963
kaibalya jena <kaibalya18j@gmail.com>
2023-01-29 11:13:25 +0530

    made the req logger

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
+const {logger} = require('./middleware/logger')
 
+//logger to log the progress and the intermediate steps
+//req logs are present in the logs folder and also in the terminal while the server in running
+app.use(logger)
+
+//enables the app to send and receieve json data
+app.use(express.json())
+
+// used build in middle-ware
+// whenever we need contents form the public folder we do not need to specify the whole path as we have it static
 app.use('/',express.static(path.join(__dirname,'/public')))
 
+//to open the root route
 app.use('/',require('./routes/root'))
 
+//to cater to eror 404
 app.all('*', (req,res)=>{
     res.status(404)
     if(req.accepts('html')){
@@ -18,6 +30,7 @@ app.all('*', (req,res)=>{
     }
 })
 
+//for setting the port and statement signifying the backend is running
 app.listen(PORT,()=>{
     console.log(`Server is running at port ${PORT}`)
 })
